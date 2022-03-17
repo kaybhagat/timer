4dd5a0b8c72713916d0c7a5b7559c8f024b471ce
kajal <kajalyb97@gmail.com>
2022-03-17 12:14:14 +0200

    Added speed functionality

--- src/components/Speed/Speed.js
+++ src/components/Speed/Speed.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Speed = ({rate}) => {
+const Speed = ({rate, speed, changeSpeed}) => {
     return (
         <div>
-            <button className = 'gray br3 w-90 pa3 mr2 bg-transparent bw1 ba b--gray hover-bg-gray hover-white'>
+            <button className = 'gray br3 w-90 pa3 mr2 bg-transparent bw1 ba b--gray hover-bg-gray hover-white active'
+                    onClick={() => changeSpeed(rate)}>
                 {rate}X
             </button>
         </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,52 +5,55 @@ import Timer from '../components/Timer/Timer';
 import Speed from '../components/Speed/Speed';
 import Control from '../components/Control/Control';
 
-// const initialState = {
-//   input:'',
-//   min:'00',
-//   sec:'00'
-// }
 
 function App() {
   
-  const [input, setInput] = useState();
+  // const [input, setInput] = useState();
   const [min, setMin] = useState('00')
   const [seconds, setSeconds] = useState('00')
   const [active, setActive] = useState(false);
   const [pause, setPause] = useState(false);
+  const [pauseLabel, setPauseLabel] = useState('Pause');
+  const [speed, setSpeed] = useState(1000);
+
 
 
   useEffect(() => {
-    const interval = setInterval (() => {
-      if(active & !pause){
-        if (seconds > 0){
+    // console.log("use effect running")
+      const interval = setInterval (() => {
+        // console.log('setInterval running')
+        if(active && !pause){
+          if (seconds > 0){
           let sec = seconds -1;
-          setSeconds(seconds => getSeconds(sec))
-          console.log('seconds >0');
-        } else if (seconds === '00'){
+          setSeconds(setFormat(sec))
+          } 
+          else if (seconds === '00'){
           let minutes = min -1;
-          setMin(min => getMin(minutes));
+          setMin(setFormat(minutes));
           setSeconds(59);
-          console.log('seconds = 0');
+          }
         }
-          // console.log('going')
-        }
-      }, 1000);
+        clearInterval(interval);
+      }, speed);
       return () => clearInterval(interval);
-    }, [active, pause, seconds, min]);
+  }, [active, pause, seconds, min, speed]);
 
   const onReset = (interval) => {
-    console.log('reset')
     clearInterval(interval);
     setMin('00');
     setSeconds('00');
     setActive(false);
     setPause(false);
+    setPauseLabel("Pause");
+    setSpeed(1000);
   }
 
   const togglePause = () => {
     setPause(!pause);
-
+    if (pause){
+      setPauseLabel('Pause')
+    } else {setPauseLabel('Resume');
+    }
   }
 
   const onInputChange = (event) => {
@@ -60,26 +63,34 @@ function App() {
 
   const onButtonStart = () => {
     setActive(true);
-    setInput('');
-    console.log('clear input');
+    // setInput(() => '');
   }
 
-  const getMin = (min) => {
-    if (min <10){
-      return '0' + min;
+  const changeSpeed = (rate) => {
+    switch (rate){
+      case '1':
+        setSpeed(1000);
+        // console.log('speed =1');
+        break;
+      case '1.5':
+        setSpeed(667);
+        // console.log('speed =1.5');
+        break;
+      case '2':
+        setSpeed(500);
+        // console.log('speed = 2');
+        break;
     }
-    return min;
+
   }
 
-  const getSeconds = (seconds) => {
-    if (seconds < 10) {
-      return '0' + seconds;
-    } 
-    return seconds;
+  const setFormat = (time) => {
+    if (time <10){
+      return '0' + time;
+    }
+    return time;
   }
 
-
-  // render () {
   return (
     <div className="App">
       <Input onInputChange={onInputChange}
@@ -87,14 +98,14 @@ function App() {
       <div>
         <Timer min={min} sec={seconds}/>
         <div className = 'flex justify-center mv3'>
-          <Control control={togglePause} label={'Pause'}/>
+          <Control control={togglePause} label={pauseLabel}/>
           <Control control={onReset} label={'Reset'}/>
         </div>
       </div>
       <div className='flex justify-center'>
-        <div><Speed rate='1'/></div>
-        <div><Speed rate='1.5'/></div>
-        <div><Speed rate='2'/></div>
+        <div><Speed rate='1'changeSpeed={changeSpeed}/></div>
+        <div><Speed rate='1.5' changeSpeed={changeSpeed}/></div>
+        <div><Speed rate='2' changeSpeed={changeSpeed}/></div>
       </div>
     </div>
   )
